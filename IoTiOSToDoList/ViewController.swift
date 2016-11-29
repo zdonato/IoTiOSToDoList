b20b0d77e17156dec89c32c9890845d934dd7720
Zachary Donato <zdonato@stevens.edu>
2016-11-29 11:24:11 -0500

    Set up basic layout for table view and cells.

--- IoTiOSToDoList/ViewController.swift
+++ IoTiOSToDoList/ViewController.swift
@@ -8,18 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoItems = [ToDoItem]()
+    
+    // MARK: - View Did Load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        // Some styles
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 50.0
+        
+        if toDoItems.count > 0 {
+            return
+        }
+        
+        toDoItems.append(ToDoItem(text: "feed the cat"))
+        toDoItems.append(ToDoItem(text: "buy eggs"))
+        toDoItems.append(ToDoItem(text: "watch WWDC videos"))
+        toDoItems.append(ToDoItem(text: "rule the Web"))
+        toDoItems.append(ToDoItem(text: "buy a new iPhone"))
+        toDoItems.append(ToDoItem(text: "darn holes in socks"))
+        toDoItems.append(ToDoItem(text: "master Swift"))
+        toDoItems.append(ToDoItem(text: "learn to draw"))
+        toDoItems.append(ToDoItem(text: "get more exercise"))
+        toDoItems.append(ToDoItem(text: "catch up with Mom"))
+        toDoItems.append(ToDoItem(text: "get a hair cut"))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Table View Data Source
+    @nonobjc func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
+        let item = toDoItems[indexPath.row]
+        
+        cell.textLabel?.text = item.text
+        
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.rowHeight
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        if (indexPath.row % 2 == 0) {
+            cell.backgroundColor = UIColor.black
+            cell.textLabel?.textColor = UIColor.white
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
 }
 
